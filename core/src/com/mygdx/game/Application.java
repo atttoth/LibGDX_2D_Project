8019d526d3dbe353b0b706f0adf3af3b964862a7
Attila Toth <att.toth1991@gmail.com>
2021-12-02 11:22:21 +0100

    Screen setup

--- core/src/com/mygdx/game/Application.java
+++ core/src/com/mygdx/game/Application.java
@@ -1,31 +1,39 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Application extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+	private OrthographicCamera orthographicCamera;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		float width = Gdx.graphics.getWidth();
+		float height = Gdx.graphics.getHeight();
+
+		orthographicCamera = new OrthographicCamera();
+		orthographicCamera.setToOrtho(false, width / 2,height / 2);
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		Gdx.gl.glClearColor(0f,0f,0f,1f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	}
-	
+
+	@Override
+	public void resize(int width, int height) {
+		orthographicCamera.setToOrtho(false, width / 2, height / 2 );
+	}
+
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }

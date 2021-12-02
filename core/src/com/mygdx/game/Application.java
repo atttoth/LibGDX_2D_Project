72a7c1e8b650e005243e98abe826cceb6704afe0
Attila Toth <att.toth1991@gmail.com>
2021-12-02 12:54:13 +0100

    Player-character graphics added

--- core/src/com/mygdx/game/Application.java
+++ core/src/com/mygdx/game/Application.java
@@ -5,20 +5,28 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.*;
 
 import static com.mygdx.game.Constants.PPM;
+import static com.mygdx.game.Constants.SCALE;
 
 
 public class Application extends ApplicationAdapter {
+
+	private boolean DEBUG = false;
 
 	private OrthographicCamera orthographicCamera;
 
 	private Box2DDebugRenderer b2dr;
 	private World world;
 	private Body player, platform;
+
+	private SpriteBatch batch;
+	private Texture texture;
 	
 	@Override
 	public void create () {
@@ -26,13 +34,16 @@ public class Application extends ApplicationAdapter {
 		float height = Gdx.graphics.getHeight();
 
 		orthographicCamera = new OrthographicCamera();
-		orthographicCamera.setToOrtho(false, width / 2,height / 2);
+		orthographicCamera.setToOrtho(false, width / SCALE,height / SCALE);
 
 		world = new World(new Vector2(0,-9.8f), false);
 		b2dr = new Box2DDebugRenderer();
 
 		player = createBox(8,10,32,32,false);
 		platform = createBox(0,0,64,32,true);
+
+		batch = new SpriteBatch();
+		texture = new Texture("character_roundRed.png");
 	}
 
 	@Override
@@ -43,18 +54,26 @@ public class Application extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0f,0f,0f,1f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+		batch.begin();
+		batch.draw(texture, player.getPosition().x * PPM - texture.getWidth() / 2,player.getPosition().y * PPM - texture.getHeight() / 2);
+		batch.end();
+
 		b2dr.render(world,orthographicCamera.combined.scl(PPM));
+		if(Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+			Gdx.app.exit();
+		}
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		orthographicCamera.setToOrtho(false, width / 2, height / 2 );
+		orthographicCamera.setToOrtho(false, width / SCALE, height / SCALE );
 	}
 
 	@Override
 	public void dispose () {
 		world.dispose();
 		b2dr.dispose();
+		batch.dispose();
 	}
 
 	public void update(float deltaTime) {
@@ -62,6 +81,7 @@ public class Application extends ApplicationAdapter {
 
 		inputUpdate(deltaTime);
 		cameraUpdate(deltaTime);
+		batch.setProjectionMatrix(orthographicCamera.combined);
 	}
 
 	public void inputUpdate(float deltaTime) {
@@ -102,7 +122,7 @@ public class Application extends ApplicationAdapter {
 		playerBody = world.createBody(bodyDef);
 
 		PolygonShape shape = new PolygonShape();
-		shape.setAsBox((float)width / 2 / PPM, (float)height / 2 / PPM);
+		shape.setAsBox((float)width / SCALE / PPM, (float)height / SCALE / PPM);
 
 		playerBody.createFixture(shape, 1.0f);
 		shape.dispose();
--- core/src/com/mygdx/game/Constants.java
+++ core/src/com/mygdx/game/Constants.java
@@ -2,4 +2,5 @@ package com.mygdx.game;
 
 public class Constants {
     public static final float PPM = 32;
+    public static final float SCALE = 2.0f;
 }
